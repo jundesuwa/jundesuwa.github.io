5f2f37dc0e2d1f73779fe2acebe21189d87ed637
darrellgjw <darrellgjw@gmail.com>
2024-07-21 03:16:55 +0800

    21/7/2024 3:16 am
    -Modified the typing test
    -Made sure that all js, html and css tests passed

--- webdeva2/js/script.js
+++ webdeva2/js/script.js
@@ -1,27 +1,26 @@
 /*
------------------------------------------------------------------------------------HEADER-------------------------------------------------------------------------------
+-----------------------------------------------------------------------------------NAV-------------------------------------------------------------------------------
 */
 
 //for nav buttons to cycle through pages
 const pageBtn = [
-  page1btn,
-  page2btn,
-  page3btn,
-  page4btn,
-  page5btn,
+  document.querySelector("#page1btn"),
+  document.querySelector("#page2btn"),
+  document.querySelector("#page3btn"),
+  document.querySelector("#page4btn"),
 ];
 
 var allpages = document.querySelectorAll(".page");
 //select all subtopic pages
 console.log(allpages);
-hideallPG1();
-function hideallPG1() { //function to hide all pages
+hideallPGNav();
+function hideallPGNav() { //function to hide all pages
   for (let onepage of allpages) { //go through all subtopic pages
-      onepage.style.display = "none"; //hide it
+    onepage.style.display = "none"; //hide it
   }
 }
-function showPG1(pgno) { //function to show selected page no
-  hideallPG1();
+function showPGNav(pgno) { //function to show selected page no
+  hideallPGNav();
   //select the page based on the parameter passed in
   let onepage = document.querySelector("#page" + pgno);
   //show the page
@@ -29,15 +28,15 @@ function showPG1(pgno) { //function to show selected page no
 }
 
 //show the main page as the initial page
-showPG1(1);
+showPGNav(1);
 
 
 /*Listen for clicks on the buttons, assign anonymous
 eventhandler functions to call show function*/
 for (let i = 0; i < pageBtn.length; i++) {
   pageBtn[i].addEventListener("click", function () {
-      var pageNumber = i + 1;
-      showPG1(pageNumber);
+    var pageNumber = i + 1;
+    showPGNav(pageNumber);
   });
 }
 
@@ -63,7 +62,7 @@ function toggleMenus() { /*open and close menu*/
 }
 
 //when screen resizes
-if(document.body.offsetWidth < 800){
+if (document.body.offsetWidth < 800) {
 
 }
 
@@ -84,7 +83,7 @@ function changeBackground() {
   headerBackgrounds[imageIndex].classList.add("shown-background");
 }
 
-//call function to change background every 3 seconds
+//call function to change background every 8 seconds
 setInterval(changeBackground, 8000);
 
 //make array of keyboard quotes for title box
@@ -95,24 +94,25 @@ const mainQuotes = [
 ];
 
 //when give title card transition effect
-const observerTitleBox = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-          entry.target.classList.add("shown-title-box");
+const observerTitleBox = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("shown-title-box");
 
-          //add a random quote to title box
-          var titleboxQuote = document.querySelector("#page1 .title-box p");
-          const randomIndex = Math.floor(Math.random() * mainQuotes.length);
-          const randomQuote = mainQuotes[randomIndex];
-          titleboxQuote.textContent = randomQuote;
-      } else {
-          entry.target.classList.remove("shown-title-box");
-      }
+      //add a random quote to title box
+      var titleboxQuote = document.querySelector("#page1 .title-box p");
+      const randomIndex = Math.floor(Math.random() * mainQuotes.length);
+      const randomQuote = mainQuotes[randomIndex];
+      titleboxQuote.textContent = randomQuote;
+    } else {
+      entry.target.classList.remove("shown-title-box");
+    }
   });
 });
 
 const elements = document.querySelectorAll("#page1 .title-box");
-elements.forEach((element) => {
+//add elements to observer
+elements.forEach(function(element) {
   observerTitleBox.observe(element);
 });
 
@@ -123,134 +123,193 @@ elements.forEach((element) => {
 */
 
 //when give info card transition effect
-const observerInfo = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-          entry.target.classList.add("shown-page2-info");
-      } else {
-          entry.target.classList.remove("shown-page2-info");
-      }
+const observerInfo = new IntersectionObserver(function (entries) {
+  entries.forEach(function (entry) {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("shown-page2-info");
+    } else {
+      entry.target.classList.remove("shown-page2-info");
+    }
   });
 });
 
+
 const elementsPage2Info = document.querySelectorAll("#page2 .info");
-elementsPage2Info.forEach((element) => {
+//add elements to observer
+elementsPage2Info.forEach(function (element) {
   observerInfo.observe(element);
 });
 
 //code for typing test from online code
-const quoteApiUrl = "https://api.quotable.io/random?minLength=80&maxLength=100";
+
+//array of random words
+const wordArray = [
+  "it", "I", "is", "am", "are",
+  "the", "a", "an", "he", "she",
+  "we", "they", "you", "this", "that",
+  "these", "those", "has", "have", "had",
+  "run", "jump", "write", "read", "eat",
+  "drink", "play", "sleep", "think", "talk",
+  "walk", "drive", "sing", "dance", "listen",
+  "speak", "hear", "see", "look", "watch",
+  "feel", "touch", "smell", "taste", "bring",
+  "take", "give", "show", "find", "make",
+  "know", "understand", "believe", "want", "need"
+];
+
+
 const quoteSection = document.getElementById("quote");
 const userInput = document.getElementById("quote-input");
+let wordCountInput = document.querySelector("#page2 #wordCount");
+
 let quote = "";
 let time = 60;
 let timer = "";
 let mistakes = 0;
-//Display random quotes
-const renderNewQuote = async () => {
-//Fetch contents from url
-const response = await fetch(quoteApiUrl);
-//Store response
-let data = await response.json();
-//Access quote
-quote = data.content;
-//Array of characters in the quote
-let arr = quote.split("").map((value) => {
-  //wrap the characters in a span tag
-  return "<span class='quote-chars'>" + value + "</span>";
-});
-//join array for displaying
-quoteSection.innerHTML += arr.join("");
-};
-//Logic for comparing input words with quote
-userInput.addEventListener("input", () => {
-let quoteChars = document.querySelectorAll(".quote-chars");
-//Create an arrat from received span tags
-quoteChars = Array.from(quoteChars);
-//array of user input characters
-let userInputChars = userInput.value.split("");
-//loop through each character in quote
-quoteChars.forEach((char, index) => {
-  //Check if char(quote character) = userInputChars[index](input character)
-  if (char.innerText == userInputChars[index]) {
-    char.classList.add("success");
+
+
+const renderNewText = function () {
+
+  //clear out the quotes string 
+  if (quote !== "") {
+    quote = "";
   }
-  //If user hasn't entered anything or backspaced
-  else if (userInputChars[index] == null) {
-    //Remove class if any
-    if (char.classList.contains("success")) {
-      char.classList.remove("success");
+
+  // Get input button for word count
+  let wordCount = parseInt(wordCountInput.value); // Convert input value to an integer
+
+  for (let i = 0; i < wordCount; i++) { // Repeat adding words up to the inputted number
+    // Get a random word from the word array
+    let randomWordIndex = Math.floor(Math.random() * wordArray.length);
+
+    // Add random word to string
+    if (i === wordCount - 1) {
+      // Don't add space at the end of the word if it's the last word
+      quote += wordArray[randomWordIndex];
     } else {
-      char.classList.remove("fail");
+      // Else add the space at the end of the word
+      quote += wordArray[randomWordIndex] + " ";
     }
+
+    //clear everything in the quoteSection div first
+    quoteSection.innerHTML = "";
+
+    //Array of characters in the quote
+    let arr = quote.split("").map(function (value) {
+      //wrap the characters in a span tag
+      return "<span class='quote-chars'>" + value + "</span>";
+    });
+
+
+    //join array for displaying
+    quoteSection.innerHTML += arr.join("");
   }
-  //If user enter wrong character
-  else {
-    //Checks if we alreasy have added fail class
-    if (!char.classList.contains("fail")) {
-      //increment and display mistakes
-      mistakes += 1;
-      char.classList.add("fail");
+
+  //debug to check quote
+  console.log(quote);
+};
+
+
+//Logic for comparing input words with quote
+userInput.addEventListener("input", function () {
+  let quoteChars = document.querySelectorAll(".quote-chars");
+  //Create an array from received span tags
+  quoteChars = Array.from(quoteChars);
+  //array of user input characters
+  let userInputChars = userInput.value.split("");
+  //loop through each character in quote
+  quoteChars.forEach(function (char, index) {
+    //Check if char(quote character) = userInputChars[index](input character)
+    if (char.innerText == userInputChars[index]) {
+      char.classList.add("success");
     }
-    document.getElementById("mistakes").innerText = mistakes;
-  }
-  //Returns true if all the characters are entered correctly
-  let check = quoteChars.every((element) => {
-    return element.classList.contains("success");
+    //If user hasn't entered anything or backspaced
+    else if (userInputChars[index] == null) {
+      //Remove class if any
+      if (char.classList.contains("success")) {
+        char.classList.remove("success");
+      } else {
+        char.classList.remove("fail");
+      }
+    }
+    //If user enter wrong character
+    else {
+      //Checks if we alreasy have added fail class
+      if (!char.classList.contains("fail")) {
+        //increment and display mistakes
+        mistakes += 1;
+        char.classList.add("fail");
+      }
+      document.getElementById("mistakes").innerText = mistakes;
+    }
+    //Returns true if all the characters are entered correctly
+    let check = quoteChars.every(function (element) {
+      return element.classList.contains("success");
+    });
+    //End test if all characters are correct
+    if (check) {
+      displayResult();
+    }
   });
-  //End test if all characters are correct
-  if (check) {
-    displayResult();
-  }
-});
 });
 //Update Timer on screen
 function updateTimer() {
-if (time == 0) {
-  //End test if timer reaches 0
-  displayResult();
-} else {
-  document.getElementById("timer").innerText = --time + "s";
-}
+  if (time == 0) {
+    //End test if timer reaches 0
+    displayResult();
+  } else {
+    document.getElementById("timer").innerText = --time + "s";
+  }
 }
 //Sets timer
-const timeReduce = () => {
-time = 60;
-timer = setInterval(updateTimer, 1000);
+const timeReduce = function () {
+  time = 60;
+  timer = setInterval(updateTimer, 1000);
 };
 //End Test
-const displayResult = () => {
-//display result div
-document.querySelector(".result").style.display = "block";
-clearInterval(timer);
-document.getElementById("stop-test").style.display = "none";
-userInput.disabled = true;
-let timeTaken = 1;
-if (time != 0) {
-  timeTaken = (60 - time) / 100;
-}
-document.getElementById("wpm").innerText =
-  (userInput.value.length / 5 / timeTaken).toFixed(2) + " wpm";
-document.getElementById("accuracy").innerText =
-  Math.round(
-    ((userInput.value.length - mistakes) / userInput.value.length) * 100
-  ) + " %";
+const displayResult = function () {
+  //display result div
+  document.querySelector(".result").style.display = "block";
+  clearInterval(timer);
+  document.getElementById("stop-test").style.display = "none";
+  userInput.disabled = true;
+  let timeTaken = 1;
+  if (time != 0) {
+    timeTaken = (60 - time) / 100;
+  }
+  document.getElementById("wpm").innerText =
+    (userInput.value.length / 5 / timeTaken).toFixed(2) + " wpm";
+  document.getElementById("accuracy").innerText =
+    Math.round(
+      ((userInput.value.length - mistakes) / userInput.value.length) * 100
+    ) + " %";
 };
 //Start Test
-const startTest = () => {
-mistakes = 0;
-timer = "";
-userInput.disabled = false;
-timeReduce();
-document.getElementById("start-test").style.display = "none";
-document.getElementById("stop-test").style.display = "block";
+const startTest = function() {
+  mistakes = 0;
+  timer = "";
+  userInput.disabled = false;
+  timeReduce();
+  document.getElementById("start-test").style.display = "none";
+  document.getElementById("stop-test").style.display = "block";
 };
-window.onload = () => {
-userInput.value = "";
-document.getElementById("start-test").style.display = "block";
-document.getElementById("stop-test").style.display = "none";
-userInput.disabled = true;
-renderNewQuote();
+window.onload = function() {
+  userInput.value = "";
+  document.getElementById("start-test").style.display = "block";
+  document.getElementById("stop-test").style.display = "none";
+  userInput.disabled = true;
+  renderNewText();
+
+  //check for any changes to word Count input, and change length of test string
+  wordCountInput.addEventListener("change", function () {
+    //clear everything in the quoteSection div first
+    quoteSection.innerHTML = "";
+
+    //Then make the new text string
+    renderNewText();
+  });
+
+
 };
 
 /*
@@ -265,7 +324,7 @@ renderNewQuote();
 
 // function hideallPG3() { //function to hide keyboard info
 //     for (let onepage of allpages) { //go through all subtopic pages
-      
+
 //     }
 // }
 
@@ -281,25 +340,25 @@ let keyboardComponentBtn = [
 var allComponentInfo = document.querySelectorAll(".box .info");
 
 function hideallComponentInfo() { //function to hide all pages
-  for (let i = 0; i < allComponentInfo.length; i++) { 
+  for (let i = 0; i < allComponentInfo.length; i++) {
 
-      //check if any of the component infos have shown-components-info class, and remove it if true
-      if(allComponentInfo[i].classList.contains("shown-components-info")){
-          allComponentInfo[i].classList.remove("shown-components-info");
-      }
+    //check if any of the component infos have shown-components-info class, and remove it if true
+    if (allComponentInfo[i].classList.contains("shown-components-info")) {
+      allComponentInfo[i].classList.remove("shown-components-info");
+    }
   }
 }
 
-function showComponentInfo(infoNumber) { //function to show selected page no
-  
+function showComponentInfo(infoNumber) { //function to show selected info no
+
   hideallComponentInfo();
 
   let infoQuery = ".diagram-info .info:nth-child(" + infoNumber + ")";
 
-  console.log(infoQuery)
+  console.log(infoQuery);
 
   let info = document.querySelector(infoQuery);
-  
+
   info.classList.add("shown-components-info");
 }
 
@@ -307,26 +366,26 @@ showComponentInfo(1);
 
 for (let i = 0; i < keyboardComponentBtn.length; i++) {
   keyboardComponentBtn[i].addEventListener("click", function () {
-      var infoNumber = i + 1;
-      showComponentInfo(infoNumber);
+    var infoNumber = i + 1;
+    showComponentInfo(infoNumber);
   });
 }
 
 //show the main page as the initial page
-showPG1(1);
+showPGNav(1);
 
 var keyboardImageList = document.querySelectorAll(".diagram-image");
 function switchDiagramImage() { /*alternate between the two images*/
 
   //check if first image has shown-image class so add class to other image
   if (keyboardImageList[0].classList.contains('shown-image')) {
-      keyboardImageList[0].classList.remove('shown-image');
-      keyboardImageList[1].classList.add('shown-image');
+    keyboardImageList[0].classList.remove('shown-image');
+    keyboardImageList[1].classList.add('shown-image');
   }
   //else it means that the other image has shown-image class so add class to first image
   else {
-      keyboardImageList[0].classList.add('shown-image');
-      keyboardImageList[1].classList.remove('shown-image');
+    keyboardImageList[0].classList.add('shown-image');
+    keyboardImageList[1].classList.remove('shown-image');
   }
 }
 
@@ -343,5 +402,5 @@ reassembleKeyboardButton.addEventListener("click", switchDiagramImage);
 -----------------------------------------------------------------------------------PAGE 4-------------------------------------------------------------------------------
 */
 
-//TODO: add interactable 
+//TODO: add interactable
 //TODO: make an interactable picture swapper
